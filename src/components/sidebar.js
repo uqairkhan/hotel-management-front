0a96da457c88541b1a78a7b3a76aa4a2524406ba
Utech LLC <utechllc@Utechs-Mac-mini-2.local>
2024-01-29 08:29:24 +0400

    backdrop sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -47,38 +47,7 @@ const pathname = usePathname()
               </a>
             </div>
             <div className="flex items-center">
-              <div className="flex items-center ms-3">
-                <div>
-                  <button onClick={onLogout} type="button" className="flex text-sm bg-gray-500 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 p-2" aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                    <span className="text-white text-xs">Logout</span>
-                    {/* <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo"/> */}
-                  </button>
-                </div>
-                <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
-                  <div className="px-4 py-3" role="none">
-                    <p className="text-sm text-gray-900 dark:text-white" role="none">
-                      Neil Sims
-                    </p>
-                    <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                      neil.sims@flowbite.com
-                    </p>
-                  </div>
-                  <ul className="py-1" role="none">
-                    <li>
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+           
             </div>
           </div>
         </div>
@@ -104,7 +73,11 @@ const pathname = usePathname()
         </div>
       </aside>
 
-
+ {/* Backdrop */}
+ {isOpenSideBar && (
+        <div className="fixed inset-0 z-30 bg-black opacity-50">
+        </div>
+      )}
 
     </>
   )
